3863c49d33ef069422acca66680b71a025e7d862
moriyama yukyo <moriyama-yukyo@alfos.jp>
2022-08-15 23:21:34 +0900

    atomsコンポーネント実装

---
+++ src/components/atoms/ShapeImage/index.stories.tsx
@@ -0,0 +1,47 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import ShapeImage from './index';
+
+export default {
+  title: 'Atoms/ShapeImage',
+  argTypes: {
+    shape: {
+      options: ['circle', 'square'],
+      control: { type: 'radio' },
+      defaultValue: '画像の形',
+      table: {
+        type: { summary: 'circle | square' },
+        defaultValue: { summary: 'square' },
+      },
+    },
+    src: {
+      control: { type: 'text' },
+      description: '画像URL',
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+    width: {
+      control: { type: 'number' },
+      defaultValue: 320,
+      description: '横幅',
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+    height: {
+      control: { type: 'number' },
+      description: '縦幅',
+      defaultValue: 320,
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+  },
+} as ComponentMeta<typeof ShapeImage>;
+
+const Template: ComponentStory<typeof ShapeImage> = (args) => (
+  <ShapeImage {...args} />
+);
+
+export const Square = Template.bind({});
+Square.args = { src: '/images/sample/1.jpeg', shape: 'square' };
---
+++ src/components/atoms/ShapeImage/index.tsx
@@ -0,0 +1,17 @@
+import { ImageSearch } from '@mui/icons-material';
+import Image, { ImageProps } from 'next/image';
+import styled from 'styled-components';
+
+type ImageShape = 'circle' | 'square';
+type ShapeImageProps = ImageProps & { shape?: ImageShape };
+
+const ImageWithShape = styled(Image)<{ shape?: ImageShape }>`
+  border-radius: ${({ shape }) => (shape === 'circle' ? '50%' : '0')};
+`;
+
+const ShapeImage = (props: ShapeImageProps) => {
+  const { shape, ...imageProps } = props;
+  return <ImageWithShape shape={shape} {...imageProps} />;
+};
+
+export default ShapeImage;
